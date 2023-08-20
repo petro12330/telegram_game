fc9f570f2e398d60231a609566b229a77025a45f
GoliusPV <GoliusPV@pgk.ru>
2023-08-20 21:05:53 +0300

    inti

--- main.js
+++ main.js
@@ -66,17 +66,17 @@ let ctx
     };
 })();
 resources.load([
-    '../../imgs/ground_bg.svg',
-    '../../imgs/ground_left.svg',
-    '../../imgs/ground_right.svg',
-    '../../imgs/base.png',
-    '../../imgs/rotate_1.png',
-    '../../imgs/rotate_2.png',
-    '../../imgs/rotate_3.png',
-    '../../imgs/rotate_4.png',
-    '../../imgs/rotate_5.png',
-    '../../imgs/rotate_6.png',
-    '../../imgs/rotate_7.png',
+    '../imgs/ground_bg.svg',
+    '../imgs/ground_left.svg',
+    '../imgs/ground_right.svg',
+    '../imgs/base.png',
+    '../imgs/rotate_1.png',
+    '../imgs/rotate_2.png',
+    '../imgs/rotate_3.png',
+    '../imgs/rotate_4.png',
+    '../imgs/rotate_5.png',
+    '../imgs/rotate_6.png',
+    '../imgs/rotate_7.png',
 ]);
 
 
